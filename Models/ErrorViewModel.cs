c3983b3d3cf983ceaedb0e291c3c2967df38e6d5
Edward Amagasaki <amagasaki.edward@gmail.com>
2023-03-28 02:26:04 -0300

    Primeiro Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _7Core_MUI_UI_Sidepanel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
